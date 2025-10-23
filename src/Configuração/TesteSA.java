c4cf27e05d5dea92bf1c98f0c179fbf6da5c1c44
Thales Oliveira <Thales930@gmail.com>
2025-10-23 11:33:25 -0300

    Questão 1 - Classe e Objeto

---
+++ src/Configuração/TesteSA.java
@@ -0,0 +1,18 @@
+package Configuração;
+
+import Entidade.Aluno;
+
+public class TesteSA {
+
+	
+		public static void main(String[] args) {
+			
+			//Criação do objeto Aluno
+			Aluno a1 = new Aluno();
+			
+			//Atribuição dos valores
+			a1.nome = "Thales";
+			a1.matricula = "123456";
+		}
+
+}
---
+++ src/Entidade/Aluno.java
@@ -0,0 +1,9 @@
+package Entidade;
+
+public class Aluno {
+	
+	//Criação dos atributos com a visibilidade publica. Onde os atributos receberão os valores em String
+	public String nome;
+	public String matricula;
+	
+}
